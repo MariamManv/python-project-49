296d31f32d7496dc456a9462eb1d6db605d8d05c
Mariam Manvelian <nimbooda979@mail.ru>
2024-03-14 02:32:55 +0300

    fixed progression?

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -8,7 +8,7 @@ MAIN_GAME_QUESTION = 'What number is missing in the progression?'
 def data():
     first_number = randint(1, 100)
     step = randint(1, 15)
-    count = 10  # количество чисел в прогрессии,указанное в условии задания
+    count = 10
     progression = list(range(first_number, first_number + count * step, step))
     correct_index = choice(range(len(progression)))
     correct_answer = str(progression[correct_index])
