92a4294656c521d1df19016f800b4f62d73480d2
Mariam Manvelian <nimbooda979@mail.ru>
2024-03-19 13:24:58 +0300

    rewriten prime_game.py

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -11,7 +11,11 @@ def data():
     if random_number < 2:
         correct_answer = False or 'no'
     else:
-        all_divisors = set(get_all_divisors(random_number))
+        all_divisors = set()
+        for i in range(1, random_number + 1):
+            if random_number % i == 0:
+                all_divisors.add(i)
         prime_divisors = {1, random_number}
-        correct_answer = all_divisors.difference(prime_divisors) == set() and 'yes' or 'no'
+        divisor_difference = all_divisors.difference(prime_divisors)
+        correct_answer = divisor_difference == set() and 'yes' or 'no'
     return random_number, correct_answer
