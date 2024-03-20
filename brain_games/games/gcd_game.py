3611e24c41d08e46da2c50370d7a69f7d9a99cab
Mariam Manvelian <nimbooda979@mail.ru>
2024-03-20 17:44:00 +0300

    gcd_game split into two functions

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -4,10 +4,9 @@ from random import randint
 MAIN_GAME_QUESTION = 'Find the greatest common divisor of given numbers.'
 
 
-def data():
+def gcd():
     random_number_1 = randint(1, 100)
     random_number_2 = randint(1, 100)
-    given_numbers = (f'{random_number_1} {random_number_2}')
     divisors_1 = set()
     for i in range(1, random_number_1 + 1):
         if random_number_1 % i == 0:
@@ -18,5 +17,11 @@ def data():
             divisors_2.add(i)
     common_divisors = divisors_1 & divisors_2
     max_divisor = max(common_divisors)
+    return random_number_1, random_number_2, max_divisor
+
+
+def data():
+    random_number_1, random_number_2, max_divisor = gcd()
+    given_numbers = (f'{random_number_1} {random_number_2}')
     correct_answer = str(max_divisor)
     return given_numbers, correct_answer
