74a8dfd0c4117d7f2f05984da865d1d85ff69d91
Mariam Manvelian <nimbooda979@mail.ru>
2024-03-20 18:04:44 +0300

    prime_game.py split into two functions

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -5,10 +5,10 @@ MAIN_GAME_QUESTION = ('Answer "yes" if given number is prime. \
 Otherwise answer "no".')
 
 
-def data():
+def prime():
     random_number = randint(1, 100)
     if random_number < 2:
-        correct_answer = False or 'no'
+        return random_number, False
     else:
         all_divisors = set()
         for i in range(1, random_number + 1):
@@ -16,5 +16,16 @@ def data():
                 all_divisors.add(i)
         prime_divisors = {1, random_number}
         divisor_difference = all_divisors.difference(prime_divisors)
-        correct_answer = divisor_difference == set() and 'yes' or 'no'
+        if divisor_difference == set():
+            return random_number, True
+        else:
+            return random_number, False
+
+
+def data():
+    random_number, answer = prime()
+    if answer == True:
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
     return random_number, correct_answer
